b731117356ee465c44b80c30a4cd5d7169b00519
Trav La Ville <18622201+DevTrav@users.noreply.github.com>
2024-09-06 12:26:02 -0500

    Adding json response in browser

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
-const  http = require('http');
+const app = require('./server')
 
-const server = http.createServer(  (req, res) => {
-    console.log(req)
-    console.log(res)
-    if (req.method === 'GET' && req.url === '/') {
-        console.log("Hello from server")
-        res.end()
-    }
-} )
-
-server.listen(3001, () => {
-    console.log(' server on  http://localhost:3001')
+app.listen(3001, () => {
+    console.log('hello on http://localhost:3001')
 })
-
